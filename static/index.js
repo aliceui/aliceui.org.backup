e37058d7bc80076082e9f8219d404025eb0d6a10
偏右 <afcstar@163.com>
2013-03-01 05:11:50 -0800

    Update documentation

---
+++ static/index.js
@@ -0,0 +1,14 @@
+seajs.use(['$', 'arale/fixed/1.0.0/fixed'],
+function($, Fixed) {
+    
+    Fixed('.side-area', 20);
+
+    $('.content h2:not(.alice-module-title)').each(function(i, item) {
+        item = $(item);
+        var list = $($('#list-template').html());
+        list.find('a').html(item.html() + list.find('a').html());
+        list.find('a').attr('href', '#' + item.attr('id'));
+        list.appendTo('.side-area');
+    });
+
+});

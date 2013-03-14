f0f26c0c93987c157c2b2d8597a9c0f3febeb44d
偏右 <afcstar@163.com>
2013-03-14 12:53:47 +0800

    优化滚动菜单效果

--- static/side.js
+++ static/side.js
@@ -15,12 +15,12 @@ define(function(require, exports, module) {
                 i = titles.length - 1;
             } else {
                 for (i=0; i<titles.length; i++) {
-                    if (top < titles.eq(i).offset().top - 20) {
-                        i--;
-                        i = (i<0) ? 0 : i;
+                    if (top < titles.eq(i).offset().top - 20) {  
                         break;
                     }
                 }
+                i--;
+                i = (i<0) ? 0 : i;
             }
             $('.side-area .side-highlight').removeClass('side-highlight');
             $('.side-area > ul > li').eq(i).addClass('side-highlight');
